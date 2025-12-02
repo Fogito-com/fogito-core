61d657aefbca7450af3bffb14842194f95361cb2
seniorshahmar <seniorshahmar@gmail.com>
2025-12-02 16:58:13 +0400

    CoreSharedCompanies

---
+++ src/Models/CoreSharedCompanies.php
@@ -0,0 +1,24 @@
+<?php
+namespace Fogito\Models;
+
+use Fogito\Config;
+
+class CoreSharedCompanies extends \Fogito\Db\RemoteModelManager
+{
+    public function __construct($properties = [])
+    {
+        foreach ($properties as $key => $value) {
+            $this->{$key} = $value;
+        }
+    }
+
+    public static function getServer()
+    {
+        return Config::getUrl("s2s");
+    }
+
+    public static function getSource()
+    {
+        return "sharedcompanies";
+    }
+}
